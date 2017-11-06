1ee785427925f97055f4bd531bd83e23cbd8e635
Felipe Augusto Romani <felipearomani@gmail.com>
2017-11-05 23:43:17 -0200

    feat: create view to show comments

--- src/components/PostDetailContainer.js
+++ src/components/PostDetailContainer.js
@@ -17,6 +17,14 @@ import {withRouter} from 'react-router-dom';
 import isEmpty from 'is-empty';
 import NotFound404 from './NotFound404';
 
+
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+import Avatar from 'material-ui/Avatar';
+import Divider from 'material-ui/Divider';
+import FlatButton from 'material-ui/FlatButton';
+import FaPlusCircle from 'react-icons/lib/fa/plus-circle';
+
 class PostDetailContainer extends Component {
 
     state = {
@@ -36,13 +44,14 @@ class PostDetailContainer extends Component {
                 this.props.addDetailedPost(post);
                 this.setState({loadPost : false})
             })
+            .catch(err => console.log(err, 'deu merrdaaaaaaaaaa'))
     }
 
     _loadComments = (postId) => {
         getCommentsByPostId(postId)
             .then(result => {
                 //aquiiii
-            });
+            })
     }
 
     onEdit = (postId) => {
@@ -70,8 +79,47 @@ class PostDetailContainer extends Component {
             <Grid fluid>
                 <Row>
                     <Col md={12} xs={12}>
-                    {isEmpty(post) && <NotFound404 message="The post you requested does not exist!" /> }
+                    {(isEmpty(post) && !this.state.loadPost) && 
+                        <NotFound404 message="The post you requested does not exist!" /> }
                     {!isEmpty(post) && <PostBody post={post} onEdit={this.onEdit} onDelete={this.onDelete} />}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12} xs={12}>
+                        <div style={{marginTop:'1em'}}>
+                            <FlatButton
+                                label="Add Comment"
+                                secondary={true}
+                                icon={<FaPlusCircle />} />
+                        </div>
+                        <Paper style={{marginTop : '0.8em'}} zDepth={1}>
+                            <List>
+                                <ListItem
+                                    disabled={true}
+                                    leftAvatar={<Avatar>A</Avatar>}>
+                                    Felipe Antunes
+                                </ListItem>
+                            </List>
+                            <Divider />
+                            <div style={{padding:'1.2em', fontSize:'0.9em'}}>
+                            asdasda
+                            </div>
+                        </Paper>
+
+                        <Paper style={{marginTop : '0.8em'}} zDepth={1}>
+                            <List>
+                                <ListItem
+                                    disabled={true}
+                                    leftAvatar={<Avatar>A</Avatar>}>
+                                    Felipe Antunes
+                                </ListItem>
+                            </List>
+                            <Divider />
+                            <div style={{padding:'1.2em', fontSize:'0.9em'}}>
+                            asdasda
+                            </div>
+                        </Paper>
+
                     </Col>
                 </Row>
             </Grid>    
